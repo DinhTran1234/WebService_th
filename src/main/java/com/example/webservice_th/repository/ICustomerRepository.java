1f7a04e6d5c455cc06b91f42ecb81e4412c4af46
Tran Dinh <trandinh@MacBook-Pro-cua-Tran.local>
2022-09-26 15:24:15 +0700

    done

---
+++ src/main/java/com/example/webservice_th/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.webservice_th.repository;
+
+import com.example.webservice_th.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/com/example/webservice_th/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.example.webservice_th.service;
+
+import com.example.webservice_th.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer>{
+}
